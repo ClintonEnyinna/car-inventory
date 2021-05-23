f91efe3dfa77ada878d6d8039efbf0b51a805132
ClintonEnyinna <enyinnaclinton@gmail.com>
2021-05-22 23:46:40 -0500

    include restfule routes for car model

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'cars#index'
+
   get    'signup', to: 'users#new'
   get    'login',  to: 'sessions#new'
   post   'login',  to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
   resources :users,   only: [:create]
+  resources :cars
 end
