a4d2e91538ebf8b92d90c2c7f82d897016938354
ClintonEnyinna <enyinnaclinton@gmail.com>
2021-05-22 21:31:58 -0500

    add session helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+  def current_user
+    return unless session[:id]
+    
+    @current_user ||= User.find(session[:id])
+  end
+
+  def log_in(user)
+    session[:id] = user.id
+  end
+
+  def log_out
+    session[:id] = nil
+    @current_user = nil
+  end
 end
