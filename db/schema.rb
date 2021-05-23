252718e9f6436e165e1abf6f04a2743125026479
ClintonEnyinna <enyinnaclinton@gmail.com>
2021-05-23 02:29:02 -0500

    add timestamp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_22_192649) do
+ActiveRecord::Schema.define(version: 2021_05_23_070635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,12 +21,16 @@ ActiveRecord::Schema.define(version: 2021_05_22_192649) do
     t.string "condition", null: false
     t.integer "price_cents", null: false
     t.bigint "dealership_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["dealership_id"], name: "index_cars_on_dealership_id"
   end
 
   create_table "dealerships", force: :cascade do |t|
     t.string "name", null: false
     t.string "address", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
